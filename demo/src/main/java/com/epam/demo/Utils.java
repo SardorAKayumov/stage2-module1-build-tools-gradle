9f510d495e12ec62572408ed1cc9bf2f62b83c14
SardorAKayumov <sardor.zeroz@gmail.com>
2022-12-22 11:30:47 +0500

    commit6

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,16 +4,21 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        if(args == null)
-            return false;
-        for(String str : args) {
-            if(str == null)
+        try {
+            if (args == null)
                 return false;
-            if(str.trim().isEmpty())
-                return false;
-            if(Double.parseDouble(str) <= 0)
-                return false;
+            for (String str : args) {
+                if (str == null)
+                    return false;
+                if (str.trim().isEmpty())
+                    return false;
+                if (Double.parseDouble(str) <= 0)
+                    return false;
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println(e);
         }
-        return true;
+        return false;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,10 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if(str == null)
-            return false;
-        if(str.trim().isEmpty())
-            return false;
-        return Double.parseDouble(str) > 0;
+        try {
+            if (str == null)
+                return false;
+            if (str.trim().isEmpty())
+                return false;
+            return Double.parseDouble(str) > 0;
+        } catch(NumberFormatException e) {
+            System.out.println(e);
+        }
+        return false;
     }
 }
