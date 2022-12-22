96f4d1a75fb4cf4a2fd49680795338ce3cb8259d
SardorAKayumov <sardor.zeroz@gmail.com>
2022-12-22 11:26:04 +0500

    commit5

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        if(args == null)
+            return false;
         for(String str : args) {
             if(str == null)
                 return false;
